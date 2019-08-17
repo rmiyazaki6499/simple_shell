dce59fe9e571c9977ae96f8b8ccc6206bd562dc5
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-08-17 15:16:20 +0000

    First iteration of replacing wrappers

--- _strncpy.c
+++ _strncpy.c
@@ -1,6 +1,19 @@
 #include "stringwrapper.h"
 
+/**
+ * _strncpy - a function that copies a string.
+ * @dest: initial string
+ * @src: string to append to dest
+ * @n: the max bytes to use
+ * Return: pointer to copied string
+ */
 char *_strncpy(char *dest, const char *src, size_t n)
 {
-	return (strncpy(dest, src, n));
+	int length;
+
+	for (length = 0; src[length] != '\0' && length < n; length++)
+		dest[length] = src[length];
+	for ( ; length < n; length++)
+		dest[length] = '\0';
+	return (dest);
 }
