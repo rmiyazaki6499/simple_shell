82a699a6195ca0c56d37ae65d76788b8b295606a
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-08-18 19:50:19 +0000

    Implemented help command

--- builtin.h
+++ builtin.h
@@ -6,36 +6,42 @@
 extern int status;
 
 /**
- * struct str_ll_s - linked list
- * @string: malloc'ed string
- * @next: points to the next node
+ * struct builtin_func_s - struct for builtin functions
+ * @func_name: name of function
+ * @function: function to be executed
  *
- * Description: singly linked list node structure
+ * Description: struct for builtin functions
  * for simple shell
  */
-typedef struct builtin_func_t
+typedef struct builtin_func_s
 {
 	char *func_name;
 	int (*function)(char **);
 } builtin_func;
 
 /**
- * struct str_ll_s - linked list
- * @string: malloc'ed string
- * @next: points to the next node
+ * struct descriptions - struct for help descriptions
+ * @func_name: name of function
+ * @description: description for help
  *
- * Description: singly linked list node structure
+ * Description: struct to manage all descriptions
  * for simple shell
  */
-typedef struct builtin_sd_t
+typedef struct descriptions_s
 {
 	char *func_name;
-	char *short_description;
-} builtin_sd;
+	void (*description)(void);
+} descriptions_t;
 
 int (*get_builtin_func(char *name))(char **name);
-char *print_d(char *name);
 
+void exit_description(void);
+void cd_description(void);
+void help_description(void);
+void setenv_description(void);
+void unsetenv_description(void);
+
+int _help(char **arguments);
 int _cd(char **arguments);
 int _printenv(char **arguments);
 int exit_(char **arguments);
