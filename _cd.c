984c8341ccca021eca508ef8c6ac635ebf42c09d
Ben Cook <808@holbertonschool.com>
2019-08-21 15:56:06 -0700

    fix memory allocation issue

--- _cd.c
+++ _cd.c
@@ -3,30 +3,34 @@
 #include "environment.h"
 #include <unistd.h>
 
-int _cd(char *argv[])
+int _cd(char **arguments)
 {
 	int status;
 	char *path;
 	char *home;
-	/* int free_path = 0; */
+	int free_path = 0;
 
 	home = _getenv("HOME");
 
-	if (!argv || !argv[0] || !argv[0][0])
+	if (!arguments || !arguments[0] || !arguments[0][0])
 		path = home;
-	else if (argv[0][0] == '~')
+	else if (arguments[0][0] == '~')
 	{
-		path = malloc(sizeof(*path) * (_strlen(home) + _strlen(argv[0]) - 1));
+		path = malloc(sizeof(*path) * (_strlen(home) + _strlen(arguments[0])));
 		if (!path)
 			return (-1);
+		free_path = 1;
 
 		path = _strcpy(path, home);
-		path = _strcat(path, argv[0] + 1);
+		path = _strcat(path, arguments[0] + 1);
 	}
 	else
-		path = argv[0];
+		path = arguments[0];
 
 	status = chdir(path);
+
+	if (free_path)
+		free(path);
 
 	return (status);
 }
