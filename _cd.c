21ecc058b70dc7390923f8199810ffb2de307c7a
Ben Cook <808@holbertonschool.com>
2019-08-26 21:33:40 -0700

    allow cd - functionality

--- _cd.c
+++ _cd.c
@@ -10,15 +10,17 @@
  */
 int _cd(char **arguments)
 {
+	static char *previous = NULL;
 	int status;
-	char *path;
-	char *home;
+	char *path, *home, *temp;
 	int free_path = 0;
 
+	if (!previous)
+		previous = getcwd(NULL, 0);
 	home = _getenv("HOME");
 
 	if (!arguments || !arguments[0] || !arguments[0][0])
-		path = home;
+		path = _strdup(home);
 	else if (arguments[0][0] == '~')
 	{
 		path = malloc(sizeof(*path) * (_strlen(home) + _strlen(arguments[0])));
@@ -29,13 +31,26 @@ int _cd(char **arguments)
 		path = _strcpy(path, home);
 		path = _strcat(path, arguments[0] + 1);
 	}
+	else if (_strcmp(arguments[0], "-") == 0)
+	{
+		path = _strdup(previous);
+		_puts(path);
+		_puts("\n");
+	}
 	else
-		path = arguments[0];
+		path = _strdup(arguments[0]);
 
+	temp = getcwd(NULL, 0);
 	status = chdir(path);
+	if (status == 0)
+	{
+		free(previous);
+		previous = temp;
+	}
+	else
+		free(temp);
 
-	if (free_path)
-		free(path);
+	free(path);
 
 	return (status);
 }
