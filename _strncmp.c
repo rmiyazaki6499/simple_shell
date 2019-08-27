22dbf1d57e14feae598a56ee36312884c2d67265
Ben Cook <808@holbertonschool.com>
2019-08-26 17:02:40 -0700

    fix strncmp for when second string is shorter than n

--- _strncmp.c
+++ _strncmp.c
@@ -10,16 +10,14 @@
  */
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char t1, t2;
+	char t1, t2;
 
-	while (n > 0)
+	while (n--)
 	{
-		t1 = (unsigned char) *s1++;
-		t2 = (unsigned char) *s2++;
-		if (t1 != t2)
+		t1 = *s1++;
+		t2 = *s2++;
+		if (t1 != t2 || t1 == '\0' || t2 == '\0')
 			return (t1 - t2);
-		if (t1 == '\0')
-			return (0);
 	}
 	return (0);
 }
